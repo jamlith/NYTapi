b43b7971903d91f4492148ad3508d4e16aa76221
jamlith <jamlith@gmail.com>
2017-08-04 14:47:47 -0400

    bees in the trap

--- assets/js/nyt.js
+++ assets/js/nyt.js
@@ -1,22 +1,33 @@
-var nyt-api-key = "5e3993ef530f4bffb71217a3629d974e"; // If you wanna sing out, sing out.
-
-function makeRow(date, headline, author, link) {
-  var row = "<tr><td>" + date + "</td><td>" + headline + "</td><td>" + author + "</td><td>" + link + "</td></tr>";
+function makeRow(date, headline, link) {
+  var row = "<tr><td>" + date + "</td><td>" + headline + "</td><td>" + link + "</td></tr>";
   return row;
 }
 function toTable(html) {
-  $("#table").html("<tr><th>Date</th><th>Headline</th><th>Author</th><th>Link</th></tr>" + html);
+  $("#output").html("<tr><th>Date</th><th>Headline</th><th>Link</th></tr>" + html);
 }
-$("#submit").click(function(event) {
-  var qry = $("#search").val();
-  var qty = $("#qty").val();
-  var start = $("#start").val();
-  var end = $("#end").val();
-  var apiurl = "http...search=" + escape(qry) + "&qty=" + qty + "&start=" + start + "&end=" + end;
-  $.get(apiurl)
-    .done(function(r) {
-      for (var i = 0; i < r.data.length; i++) {
-        
-      }
+$(document).ready(function(){
+  $("#submit").click(function(event) {
+    console.log("click");
+    var qry = $("#search").val();
+    var qty = $("#qty").val();
+    var start = $("#start").val() + "0101";
+    var end = $("#end").val() + "0101";
+    var apiurl = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    apiurl+= '?' + $.param({
+      'api-key': "5e3993ef530f4bffb71217a3629d974e",
+      'q' : $("#search").val(),
+      'begin_date': $("#start").val() + "0101",
+      'end_date': $("#end").val() + "0101"
     });
+    console.log(apiurl);
+    $.get(apiurl)
+      .done(function(r) {
+        var rows;
+        for(var i = 0; i < qty; i++){
+          rows += makeRow(r.response.docs[i].pub_date, r.response.docs[i].headline.main, r.response.docs[i].web_url);
+        }
+        toTable(rows);
+      });
+
+  });
 });
