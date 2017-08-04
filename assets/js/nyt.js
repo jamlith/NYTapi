09a7bed28c5ca16755cc5941c22aadfa0efe491a
jamlith <jamlith@gmail.com>
2017-08-04 14:02:28 -0400

    mostly done with blah blah

--- assets/js/nyt.js
+++ assets/js/nyt.js
@@ -1,1 +1,22 @@
 var nyt-api-key = "5e3993ef530f4bffb71217a3629d974e"; // If you wanna sing out, sing out.
+
+function makeRow(date, headline, author, link) {
+  var row = "<tr><td>" + date + "</td><td>" + headline + "</td><td>" + author + "</td><td>" + link + "</td></tr>";
+  return row;
+}
+function toTable(html) {
+  $("#table").html("<tr><th>Date</th><th>Headline</th><th>Author</th><th>Link</th></tr>" + html);
+}
+$("#submit").click(function(event) {
+  var qry = $("#search").val();
+  var qty = $("#qty").val();
+  var start = $("#start").val();
+  var end = $("#end").val();
+  var apiurl = "http...search=" + escape(qry) + "&qty=" + qty + "&start=" + start + "&end=" + end;
+  $.get(apiurl)
+    .done(function(r) {
+      for (var i = 0; i < r.data.length; i++) {
+        
+      }
+    });
+});
